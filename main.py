01d73b7f0ceb0827e7a3f73c2988a7099104f606
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-06-26 19:02:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import datetime
 import os
 import shutil
 import googleapiclient.errors
-from googleapiclient.discovery import build #pip install google-api-python-client
-from google_auth_oauthlib.flow import InstalledAppFlow #pip install google-auth-oauthlib
+from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+import json
 import config
+import subprocess
 
 num_to_month = {
     1: "Jan",
@@ -26,124 +27,93 @@ num_to_month = {
     10: "Oct",
     11: "Nov",
     12: "Dec"
-} 
+}
 
-# USER VARIABLES FILL THESE OUT (fill out username and password in config.py)
 IG_USERNAME = config.IG_USERNAME
 IG_PASSWORD = config.IG_PASSWORD
-print(IG_USERNAME)
-print(IG_PASSWORD)
+
 title = "TRY NOT TO LAUGH (BEST Dank video memes) V1"
 now = datetime.datetime.now()
 videoDirectory = "./DankMemes_" + num_to_month[now.month].upper() + "_" + str(now.year) + "_V" + str(now.day) + "/"
 outputFile = "./" + num_to_month[now.month].upper() + "_" + str(now.year) + "_v" + str(now.day) + ".mp4"
 
-INTRO_VID = '' # SET AS '' IF YOU DONT HAVE ONE
+INTRO_VID = ''
 OUTRO_VID = ''
 TOTAL_VID_LENGTH = 13*60
 MAX_CLIP_LENGTH = 19
 MIN_CLIP_LENGTH = 5
 DAILY_SCHEDULED_TIME = "20:00"
-TOKEN_NAME = "token.json" # Don't change
+TOKEN_NAME = "token.json"
 
-# Setup Google 
 SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-client_secrets_file = "googleAPI.json"
 
 def routine():
-
-    # Handle GoogleAPI oauthStuff
-    print("Handling GoogleAPI")
     creds = None
-    # The file token1.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists(TOKEN_NAME):
-        creds = Credentials.from_authorized_user_file(TOKEN_NAME, SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+        with open(TOKEN_NAME, 'r') as token:
+            creds_data = json.load(token)
+            creds = Credentials.from_authorized_user_info(creds_data, SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                client_secrets_file, SCOPES)
-            creds = flow.run_console()
-        # Save the credentials for the next run
+            raise Exception("No valid credentials found.")
         with open(TOKEN_NAME, 'w') as token:
             token.write(creds.to_json())
 
     googleAPI = build('youtube', 'v3', credentials=creds)
 
     now = datetime.datetime.now()
-    print(now.year, now.month, now.day, now.hour, now.minute, now.second)
-    
-    #metadataFile = "./metadata-" + num_to_month[now.month].upper() + "_" + str(now.year) + "_v" + str(now.day) + ".txt"
     description = ""
-    print(outputFile)
-
     if not os.path.exists(videoDirectory):
         os.makedirs(videoDirectory)
-    
-    # Step 1: Scrape Videos
-    print("Scraping Videos...")
-    scrapeVideos(username = IG_USERNAME,
-                 password = IG_PASSWORD,
-                 output_folder = videoDirectory,
-                  days=1)
-    print("Scraped Videos!")
-    
-    description = "Enjoy the memes! :) \n\n" \
-    "like and subscribe to @Chewy for more \n\n" \
 
-    # Step 2: Make Compilation
-    print("Making Compilation...")
-    makeCompilation(path = videoDirectory,
-                    introName = INTRO_VID,
-                    outroName = OUTRO_VID,
-                    totalVidLength = TOTAL_VID_LENGTH,
-                    maxClipLength = MAX_CLIP_LENGTH,
-                    minClipLength = MIN_CLIP_LENGTH,
-                    outputFile = outputFile)
-    print("Made Compilation!")
-    
+    scrapeVideos(username=IG_USERNAME, password=IG_PASSWORD, output_folder=videoDirectory, days=1)
+
+    description = "Enjoy the memes! :) \n\nlike and subscribe to @Chewy for more \n\n"
+
+    makeCompilation(path=videoDirectory, introName=INTRO_VID, outroName=OUTRO_VID,
+                    totalVidLength=TOTAL_VID_LENGTH, maxClipLength=MAX_CLIP_LENGTH,
+                    minClipLength=MIN_CLIP_LENGTH, outputFile=outputFile)
+
     description += "\n\nCopyright Disclaimer, Under Section 107 of the Copyright Act 1976, allowance is made for 'fair use' for purposes such as criticism, comment, news reporting, teaching, scholarship, and research. Fair use is a use permitted by copyright statute that might otherwise be infringing. Non-profit, educational or personal use tips the balance in favor of fair use.\n\n"
     description += "#memes #dankmemes #compilation #funny #funnyvideos \n\n"
 
-    # Step 3: Upload to Youtube
-    print("Uploading to Youtube...")
-    uploadYtvid(VIDEO_FILE_NAME=outputFile,
-                title=title,
-                description=description,
-                googleAPI=googleAPI)
-    print("Uploaded To Youtube!")
-    
-    # Step 4: Cleanup
-    print("Removing temp files!")
-    # Delete all files made:
-    #   Folder videoDirectory
+    uploadYtvid(VIDEO_FILE_NAME=outputFile, title=title, description=description, googleAPI=googleAPI)
+
     shutil.rmtree(videoDirectory, ignore_errors=True)
-    #   File outputFile
     try:
         os.remove(outputFile)
-    except OSError as e:  ## if failed, report it back to the user ##
-        print ("Error: %s - %s." % (e.filename, e.strerror))
-    print("Removed temp files!")
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+
+    # Commit and push changes to GitHub
+    commit_and_push_changes()
+
+def commit_and_push_changes():
+    try:
+        subprocess.run(["git", "config", "--global", "user.name", "github-actions[bot]"], check=True)
+        subprocess.run(["git", "config", "--global", "user.email", "github-actions[bot]@users.noreply.github.com"], check=True)
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", "Automated update"], check=True)
+        subprocess.run(["git", "push", "https://${GH_PAT}@github.com/${{ github.repository }}.git", "HEAD:main"], check=True)
+        print("Changes pushed to GitHub successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to push changes to GitHub: {e}")
 
 def attemptRoutine():
-    while(1):
+    while True:
         try:
             routine()
             break
         except OSError as err:
-            print("Routine Failed on " + "OS error: {0}".format(err))
-            time.sleep(60*60)
+            print("Routine Failed on OS error: {0}".format(err))
+            time.sleep(60 * 60)
 
-#attemptRoutine()
 schedule.every().day.at(DAILY_SCHEDULED_TIME).do(attemptRoutine)
 
 attemptRoutine()
 while True:
-    schedule.run_pending()  
-    time.sleep(60) # wait one min
-
+    schedule.run_pending()
+    time.sleep(60)
