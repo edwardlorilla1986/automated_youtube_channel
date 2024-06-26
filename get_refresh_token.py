e00e42307e73578fbf0a13da217b63c90ca2ec44
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-06-26 19:22:44 +0800

    Update get_refresh_token.py

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -1,20 +1,14 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
-import json
-import os
 
 SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
 def get_credentials():
-    client_secrets_file = "googleAPI.json"
-    flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'googleAPI.json', SCOPES)
     creds = flow.run_local_server(port=0)
-    with open("token.json", "w") as token:
+    with open('token.json', 'w') as token:
         token.write(creds.to_json())
-    print("Credentials saved to token.json")
-
-    return creds
+    print("Refresh Token:", creds.refresh_token)
 
 if __name__ == "__main__":
-    creds = get_credentials()
-    print("Refresh Token:", creds.refresh_token)
+    get_credentials()
